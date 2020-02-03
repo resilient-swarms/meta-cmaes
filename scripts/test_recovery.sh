4ec651e774f0445c480040dfc4f29edd361ce8de
bossdm <davidmbossens@gmail.com>
2020-02-03 23:12:49 +0000

    correct test_recovery.sh for envir_meta

--- scripts/test_recovery.sh
+++ scripts/test_recovery.sh
@@ -2,12 +2,12 @@
 
 DEST=$1 # destination folder
 
-for method in bo; do
+for method in meta; do
   for test_type in envir; do
     
     
     if [[ "${method}" == "meta" ]]; then
-      if [[ "${method}" == "envir_meta" ]]; then
+      if [[ "${test_type}" == "envir" ]]; then
         last_gen=270  # leads to 2.8M evals
       else
         last_gen=260  # leads to 2.8M evals
@@ -24,7 +24,7 @@ for method in bo; do
       Outfolder="${DEST}/${method_tag}/exp${replicate}"
       echo "will write to ${Outfolder}"
 
-      ${SFERES_DIR}/build/exp/MAP-Elites-Rhex/test_${binary_tag}_binary --load ${Outfolder}/gen_${last_gen} --d ${Outfolder} -o ${Outfolder}/${test_type}_PARALLELperformance &
+      ${SFERES_DIR}/build/exp/MAP-Elites-Rhex/test_${binary_tag}_binary --load ${Outfolder}/gen_${last_gen} --d ${Outfolder} -o ${Outfolder}/${test_type}_performance &
     done
 
   done
